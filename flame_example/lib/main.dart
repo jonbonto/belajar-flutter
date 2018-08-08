9d9801fe6ccea8ac764e5e9bdc6a3f7f4782cfaa
Mukhammad Khalimudin <khalimudin@gmail.com>
2018-08-08 19:34:04 +0700

    using initialDimensions

--- flame_example/lib/main.dart
+++ flame_example/lib/main.dart
@@ -3,16 +3,19 @@ import 'dart:ui';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 
-void main() {
+void main()  async {
   Flame.util.enableEvents();
   Flame.audio.disableLog();
-  new MyGame().start();
+  var dimensions = await Flame.util.initialDimensions();
+  new MyGame(dimensions).start();
 }
 
 class MyGame extends Game {
+  Size dimensions;
+  MyGame(this.dimensions);
   @override
   void render(Canvas canvas) {
-    var rect = new Rect.fromLTWH(10.0, 10.0, 100.0, 100.0);
+    var rect = new Rect.fromLTWH(100.0, 100.0, dimensions.width - 200.0, dimensions.height - 200.0);
     var paint = new Paint()..color = new Color(0xFFFF0000);
     canvas.drawRect(rect, paint);
   }
